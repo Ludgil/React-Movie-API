fdf94a80d12d49e1d14641f40cecbc1d9a910665
unknown <gilotaux.lud@gmail.com>
2021-02-02 18:04:11 +0100

    modif app.js

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import { useFetch } from "./functions/fetch";
 import Home from "./pages/Home";
 import Info from "./pages/Info";
 import Error from "./pages/Error";
-import { key } from "./functions/key";
+import { api_key } from "./functions/key";
 
-const url = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${key}&page=1`;
-const url2 = `https://api.themoviedb.org/3/search/movie?api_key=${key}&query=`;
+const url = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${api_key}&page=1`;
+const url2 = `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query=`;
 
 function App() {
   const [searchMovie, setSearchMovie] = useState("");
